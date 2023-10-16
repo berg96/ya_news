8e49067d07e977d54ebba3627ffc6da9d502b28b
Artem Kulikov <chigar2010@yandex.ru>
2023-10-17 00:11:50 +0200

    add 3 and 5 tests

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -8,17 +8,29 @@
 # Импортируем класс HTTPStatus.
 from http import HTTPStatus
 
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 # Импортируем функцию reverse().
 from django.urls import reverse
 
-from news.models import News
+from news.models import Comment,News
+
+User = get_user_model()
 
 
 class TestRoutes(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.news = News.objects.create(title='Заголовок', text='Текст')
+        # Создаём двух пользователей с разными именами:
+        cls.author = User.objects.create(username='Лев Толстой')
+        cls.reader = User.objects.create(username='Читатель простой')
+        # От имени одного пользователя создаём комментарий к новости:
+        cls.comment = Comment.objects.create(
+            news=cls.news,
+            author=cls.author,
+            text='Текст комментария'
+        )
 
     def test_pages_availability(self):
         # Создаём набор тестовых данных - кортеж кортежей.
@@ -46,3 +58,19 @@ class TestRoutes(TestCase):
                 url = reverse(name, args=args)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_availability_for_comment_edit_and_delete(self):
+        users_statuses = (
+            (self.author, HTTPStatus.OK),
+            (self.reader, HTTPStatus.NOT_FOUND),
+        )
+        for user, status in users_statuses:
+            # Логиним пользователя в клиенте:
+            self.client.force_login(user)
+            # Для каждой пары "пользователь - ожидаемый ответ"
+            # перебираем имена тестируемых страниц:
+            for name in ('news:edit', 'news:delete'):
+                with self.subTest(user=user, name=name):
+                    url = reverse(name, args=(self.comment.id,))
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
