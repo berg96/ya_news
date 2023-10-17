5bfaa9edd79a82f8581ef6d94801ad54734f7e83
Artem Kulikov <chigar2010@yandex.ru>
2023-10-17 20:33:34 +0200

    add tests

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -1,17 +1,17 @@
-# Количество новостей на главной странице — не более 10.
-# Новости отсортированы от самой свежей к самой старой. Свежие новости в начале списка.
-# Комментарии на странице отдельной новости отсортированы от старых к новым: старые в начале списка, новые — в конце.
-# Анонимному пользователю недоступна форма для отправки комментария на странице отдельной новости, а авторизованному доступна.
 from datetime import datetime, timedelta
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
 
-from news.models import News
+from news.models import Comment, News
+
+User = get_user_model()
 
 
-class TestContent(TestCase):
+class TestHomePage(TestCase):
     HOME_URL = reverse('news:home')
 
     @classmethod
@@ -46,3 +46,50 @@ class TestContent(TestCase):
         sorted_dates = sorted(all_dates, reverse=True)
         # Проверяем, что исходный список был отсортирован правильно.
         self.assertEqual(all_dates, sorted_dates)
+
+
+class TestDetailPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(
+            title='Тестовая новость', text='Просто текст.'
+        )
+        # Сохраняем в переменную адрес страницы с новостью:
+        cls.detail_url = reverse('news:detail', args=(cls.news.id,))
+        cls.author = User.objects.create(username='Комментатор')
+        # Запоминаем текущее время:
+        now = timezone.now()
+        # Создаём комментарии в цикле.
+        for index in range(2):
+            # Создаём объект и записываем его в переменную.
+            comment = Comment.objects.create(
+                news=cls.news, author=cls.author, text=f'Tекст {index}',
+            )
+            # Сразу после создания меняем время создания комментария.
+            comment.created = now + timedelta(days=index)
+            # И сохраняем эти изменения.
+            comment.save()
+
+    def test_comments_order(self):
+        response = self.client.get(self.detail_url)
+        # Проверяем, что объект новости находится в словаре контекста
+        # под ожидаемым именем - названием модели.
+        self.assertIn('news', response.context)
+        # Получаем объект новости.
+        news = response.context['news']
+        # Получаем все комментарии к новости.
+        all_comments = news.comment_set.all()
+        # Проверяем, что время создания первого комментария в списке
+        # меньше, чем время создания второго.
+        self.assertLess(all_comments[0].created, all_comments[1].created)
+
+    def test_anonymous_client_has_no_form(self):
+        response = self.client.get(self.detail_url)
+        self.assertNotIn('form', response.context)
+
+    def test_authorized_client_has_form(self):
+        # Авторизуем клиент при помощи ранее созданного пользователя.
+        self.client.force_login(self.author)
+        response = self.client.get(self.detail_url)
+        self.assertIn('form', response.context)
