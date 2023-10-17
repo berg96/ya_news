ac6ff6ba16b9796c332d3a6d18356fc056661fe3
Artem Kulikov <chigar2010@yandex.ru>
2023-10-17 12:17:16 +0200

    add test

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -2,6 +2,7 @@
 # Новости отсортированы от самой свежей к самой старой. Свежие новости в начале списка.
 # Комментарии на странице отдельной новости отсортированы от старых к новым: старые в начале списка, новые — в конце.
 # Анонимному пользователю недоступна форма для отправки комментария на странице отдельной новости, а авторизованному доступна.
+from datetime import datetime, timedelta
 
 from django.conf import settings
 from django.test import TestCase
@@ -15,8 +16,13 @@ class TestContent(TestCase):
 
     @classmethod
     def setUpTestData(cls):
+        today = datetime.today()
         News.objects.bulk_create(
-            News(title=f'Новость {index}', text='Просто текст.')
+            News(
+                title=f'Новость {index}',
+                text='Просто текст.',
+                date=today - timedelta(days=index)
+            )
             for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
         )
 
@@ -30,3 +36,13 @@ class TestContent(TestCase):
         news_count = len(object_list)
         # Проверяем, что на странице именно 10 новостей.
         self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
+
+    def test_news_order(self):
+        response = self.client.get(self.HOME_URL)
+        object_list = response.context['object_list']
+        # Получаем даты новостей в том порядке, как они выведены на странице.
+        all_dates = [news.date for news in object_list]
+        # Сортируем полученный список по убыванию.
+        sorted_dates = sorted(all_dates, reverse=True)
+        # Проверяем, что исходный список был отсортирован правильно.
+        self.assertEqual(all_dates, sorted_dates)
